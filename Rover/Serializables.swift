1ebf622987d8169d2b26bd49e59be6a3403c495f
Sean Rucker <sean@rover.io>
2017-01-23 14:18:30 -0500

    Up version number

--- Rover/Serializables.swift
+++ Rover/Serializables.swift
@@ -244,7 +244,7 @@ extension Device : Serializable {
             "manufacturer": "Apple",
             "os-version": osVersionString,
             "model": self.platform(),
-            "sdk-version": "1.5.0",
+            "sdk-version": "1.6.0-beta.1",
             "gimbal-mode": gimbalMode,
             "development": isDevelopment
         ]

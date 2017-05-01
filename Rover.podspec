8571570215154715cc3b3ebacb005510224dc6cd
Sean Rucker <sean@rover.io>
2017-05-01 17:01:03 -0400

    v1.6.4-beta.1

--- Rover.podspec
+++ Rover.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "Rover"
-  s.version      = "1.6.3"
+  s.version      = "1.6.4-beta.1"
   s.summary      = "iOS framework for the Rover platform"
   s.homepage     = "https://www.rover.io"
   s.license      = "Apache License, Version 2.0"
--- Rover/Serializables.swift
+++ Rover/Serializables.swift
@@ -244,7 +244,7 @@ extension Device : Serializable {
             "manufacturer": "Apple",
             "os-version": osVersionString,
             "model": self.platform(),
-            "sdk-version": "1.6.3",
+            "sdk-version": "1.6.4-beta.1",
             "gimbal-mode": gimbalMode,
             "development": isDevelopment
         ]

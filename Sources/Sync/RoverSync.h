264b05da7904c4f25e79d0636577e21d19f6d6b8
Andrew Clunis <andrew@rover.io>
2018-11-16 11:31:38 -0500

    Create Sync module/target.

---
+++ Sources/Sync/RoverSync.h
@@ -0,0 +1,19 @@
+//
+//  RoverSync.h
+//  RoverSync
+//
+//  Created by Andrew Clunis on 2018-11-16.
+//  Copyright © 2018 Rover Labs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RoverSync.
+FOUNDATION_EXPORT double RoverSyncVersionNumber;
+
+//! Project version string for RoverSync.
+FOUNDATION_EXPORT const unsigned char RoverSyncVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoverSync/PublicHeader.h>
+
+

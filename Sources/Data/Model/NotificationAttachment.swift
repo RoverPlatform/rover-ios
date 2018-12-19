49544ead713ae597e2248cea4eb9f12f3ad9959b
Andrew Clunis <andrew@orospakr.ca>
2018-12-18 22:09:12 -0500

    NoticationAttachment NSCoding.

--- Sources/Data/Model/NotificationAttachment.swift
+++ Sources/Data/Model/NotificationAttachment.swift
@@ -7,8 +7,35 @@
 //
 
 import Foundation
+import os
 
-public struct NotificationAttachment: Codable, Equatable {
+public class NotificationAttachment: NSObject, Codable, NSCoding {
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.format.rawValue, forKey: "format")
+        aCoder.encode(self.url, forKey: "url")
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        guard let formatString = aDecoder.decodeObject(forKey: "format") as? String else {
+            os_log("NotificationAttachment: Format field missing from NSCoder.", log: .persistence, type: .error)
+            return nil
+        }
+        guard let format = Format(rawValue: formatString) else {
+            os_log("NotificationAttachment: Invalid format field in NSCoder.", log: .persistence, type: .error)
+            return nil
+        }
+        self.format = format
+        guard let urlString = aDecoder.decodeObject(forKey: "url") as? String else {
+            os_log("NotificationAttachment: URL field missing from NSCoder.", log: .persistence, type: .error)
+            return nil
+        }
+        guard let url = URL(string: urlString) else {
+            os_log("NotificationAttachment: URL field invalid from NSCoder.", log: .persistence, type: .error)
+            return nil
+        }
+        self.url = url
+    }
+    
     public enum Format: String, Codable {
         case audio = "AUDIO"
         case image = "IMAGE"

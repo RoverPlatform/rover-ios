fe6dc2f64e0b279c553bc323e4ff8fbc2da1cf55
Andrew Clunis <andrew@orospakr.ca>
2019-01-07 08:29:32 -0500

    modeling WIP

---
+++ Sources/Data/Model/Campaigns/Campaign.swift
@@ -0,0 +1,23 @@
+//
+//  Campaign.swift
+//  RoverData
+//
+//  Created by Andrew Clunis on 2019-01-04.
+//  Copyright © 2019 Rover Labs Inc. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Campaign : NSManagedObject, Codable {
+    
+}
+
+
+class TimeInterval: NSCoding, Codable {
+        // value object type, really just a simple tuple,
+    
+    // so intead of a full NSManagedObject I'll do NSCoding+Transformable.
+    
+    
+}

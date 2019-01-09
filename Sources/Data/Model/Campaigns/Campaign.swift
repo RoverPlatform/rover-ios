786f78c5c9752fc7b638486fa6d257d62c2e7b82
Andrew Clunis <andrew@rover.io>
2019-01-09 15:15:27 -0500

    continued WIP

--- Sources/Data/Model/Campaigns/Campaign.swift
+++ Sources/Data/Model/Campaigns/Campaign.swift
@@ -13,15 +13,19 @@ class Predicate : NSManagedObject {
     // abstract.
 }
 
-@objc public enum ComparisonPredicateModifier : Int {
+@objc public enum ComparisonPredicateModifier : Int, Codable {
     case direct
     case any
     case all
     
-    // TODO: Codable.
+    enum CodingKeys : String, CodingKey {
+        case direct
+        case any
+        case all
+    }
 }
 
-@objc public enum ComparisonPredicateOperator : Int {
+@objc public enum ComparisonPredicateOperator : Int, Codable {
     case lessThan
     case lessThanOrEqualTo
     case greaterThan
@@ -36,25 +40,108 @@ class Predicate : NSManagedObject {
     case between
     case geoWithin
     
-    // TODO: Codable
+    enum CodingKeys : String, CodingKey {
+        case lessThan
+        case lessThanOrEqualTo
+        case greaterThan
+        case greaterThanOrEqualTo
+        case equalTo
+        case notEqualTo
+        case like
+        case beginsWith
+        case endsWith
+        case in_ = "in"
+        case contains
+        case between
+        case geoWithin
+    }
 }
 
-@objc public enum CompoundPredicateLogicalType : Int {
+@objc public enum CompoundPredicateLogicalType : Int, Codable {
     case or
     case and
     case not
     
-    // TODO: Codable
+    enum CodingKeys : String, CodingKey {
+        case or
+        case and
+        case not
+    }
 }
 
-class ComparisonPredicate: Predicate {
+class ComparisonPredicate: Predicate, Codable {
     @NSManaged public internal(set) var keyPath: String
     @NSManaged public internal(set) var modifier: ComparisonPredicateModifier
     @NSManaged public internal(set) var op: ComparisonPredicateOperator
-    @NSManaged public internal(set) var value: NSObject // could be any of Float, String, DateTime, Number, or NSArrays thereof.  Booleans are also possible, but will just appear as Number.
+    @NSManaged public internal(set) var value: NSObject // could be any of NSNumber, NSString, NSDate, or NSArrays thereof.  Booleans are also possible, but will just appear as Number. TODO maybe need to use same arrangement for booleans as we did with Attributes.
     
-    // TODO: Codable
+    enum CodingKeys : String, CodingKey {
+        case keyPath
+        case modifier
+        case op = "operator"
+        
+        case numberValue
+        case numberValues
+        case stringValue
+        case stringValues
+        case booleanValue
+        case booleanValues
+        case dateTimeValue
+        case dateTimeValues
+    }
     
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.keyPath = try container.decode(String.self, forKey: .keyPath)
+        self.modifier = try container.decode(ComparisonPredicateModifier.self, forKey: .modifier)
+        self.op = try container.decode(ComparisonPredicateOperator.self, forKey: .op)
+        
+        // On the GraphQL side ComparisonPredicateValue is basically a union of all possible value types, but only one
+        // may be filled in.  Those value types are all primitives OR arrays thereof.  All of those can be represented with NSObjects (NSArrays, NSNumber, etc.), all of which support NSCoding, so unlike the GraphQL side they'll work very nicely as a single field here.
+
+        
+        if container.contains(.numberValue) {
+            self.value = NSNumber(value: try container.decode(Double.self, forKey: .numberValue))
+        } else if container.contains(.numberValues) {
+            self.value = NSArray(array: try container.decode([Double].self, forKey: .numberValues).map({ double in
+                return NSNumber(value: double)
+            }))
+        } else if container.contains(.stringValue) {
+            self.value = NSString(string: try container.decode(String.self, forKey: .stringValue))
+        } else if container.contains(.stringValues) {
+            self.value = NSArray(array: try container.decode([String].self, forKey: .stringValues).map({ string in
+                return NSString(string: string)
+            }))
+        } else if container.contains(.booleanValue) {
+            self.value = NSNumber(value: try container.decode(Bool.self, forKey: .booleanValue))
+        } else if container.contains(.booleanValues) {
+            self.value = NSArray(array: try container.decode([Bool].self, forKey: .booleanValues).map({ bool in
+                return BooleanValue(bool)
+            }))
+        } else if container.contains(.dateTimeValue) {
+            self.value = try container.decode(Date.self, forKey: .dateTimeValue) as NSDate
+        } else if container.contains(.dateTimeValues) {
+            self.value = NSArray(array: try container.decode([Date].self, forKey: .dateTimeValues).map({ date in
+                date as NSDate
+            }))
+        } else {
+            let context = DecodingError.Context(codingPath: [], debugDescription: "Missing one of numberValue, numberValues, stringValue, stringValues, booleanValue, booleanValues, dateTimeValue, dateTimeValues")
+            throw DecodingError.dataCorrupted(context)
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.keyPath, forKey: .keyPath)
+        try container.encode(self.modifier, forKey: .modifier)
+        try container.encode(self.op, forKey: .op)
+        
+        switch value {
+        case let number as NSNumber:
+            try container.encode(number.doubleValue, forKey: .numberValue)
+            case let numbers as NSArray<NS
+        }
+    }
 }
 
 class CompoundPredicate: Predicate {

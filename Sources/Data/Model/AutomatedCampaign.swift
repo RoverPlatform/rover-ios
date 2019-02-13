a76ba914243481ed67b83ea87d074152a8b13cfe
Andrew Clunis <andrew@rover.io>
2019-02-13 17:02:52 -0500

    building accessors for campaign NSManagedObject data elements

--- Sources/Data/Model/AutomatedCampaign.swift
+++ Sources/Data/Model/AutomatedCampaign.swift
@@ -20,15 +20,28 @@ public final class AutomatedCampaign: Campaign {
     // flattened trigger filters:
     // day of week:
     
-    // dayOfWeekFilterMonday: Bool?
-    // dayOfWeekFilterTuesday: Bool?
-    // dayOfWeekFilterWednesday: Bool?
-    // dayOfWeekFilterThursday: Bool?
-    // dayOfWeekFilterFriday: Bool?
-    // dayOfWeekFilterSaturday: Bool?
-    // dayOfWeekFilterSunday: Bool?
     
-    // timeOfDayFilterStartTime: Int?
+
+    public private(set) var dayOfWeekFilterMonday: Bool? {
+        get {
+            return getOptionalBooleanForPrimitiveField(forKey: dayOfWeekFilterMondayFieldName)
+        }
+        set {
+            setOptionalBooleanForPrimitiveField(newValue: newValue, forKey: "dayOfWeekFilterMondayFieldName")
+        }
+    }
+    
+    
+    
+    // dayOfWeekFilterTuesday: BooleanValue?
+    // dayOfWeekFilterWednesday: BooleanValue?
+    // dayOfWeekFilterThursday: BooleanValue?
+    // dayOfWeekFilterFriday: BooleanValue?
+    // dayOfWeekFilterSaturday: BooleanValue?
+    // dayOfWeekFilterSunday: BooleanValue?
+    
+    // TODO: ANDREW START HERE
+    @NSManaged public var timeOfDayFilterStartTime: Int?
     // timeOfDayFilterEndTime: Int?
     
     //
@@ -38,66 +51,104 @@ public final class AutomatedCampaign: Campaign {
         return AutomatedCampaign(context: context)
     }
     
-    // Event Attributes filter predicate
-    
-    // Segment filter predicate
-    
-    //
-    public private(set) var eventAttributeFilterPredicate: Predicate? {
+    public private(set) var triggerSegmentPredicate: Predicate? {
         get {
-            let key = eventAttributeFilterPredicateFieldName
-            self.willAccessValue(forKey: key)
-            defer { self.didAccessValue(forKey: key) }
-            guard let primitiveValue = primitiveValue(forKey: key) as? Data else {
-                return nil
-            }
-            
-            guard let predicateType = try? JSONDecoder.default.decode(PredicateType.self, from: primitiveValue) else {
-                os_log("Unable to determine type of predicate stored in Core Data.", log: .persistence, type: .error)
-                return nil
-            }
-            
-            do {
-                switch predicateType {
-                case .comparisonPredicate:
-                    return try JSONDecoder.default.decode(ComparisonPredicate.self, from: primitiveValue)
-                case .compoundPredicate:
-                    return try JSONDecoder.default.decode(CompoundPredicate.self, from: primitiveValue)
-                }
-            } catch {
-                os_log("Unable to decode predicate stored in core data: %s", log: .persistence, type: .error, String(describing: error))
-                return nil
-            }
+            return getPredicateForPrimitiveField(forKey: triggerSegmentPredicateFieldName)
         }
         set {
-            let key = eventAttributeFilterPredicateFieldName
-            willChangeValue(forKey: key)
-            defer { didChangeValue(forKey: key) }
-            let primitiveValue: Data
-            
-            guard let newValue = newValue else {
-                setPrimitiveValue(nil, forKey: key)
-                return
-            }
+            setPrimitiveValue(newValue, forKey: triggerSegmentPredicateFieldName)
+        }
+    }
 
-            do {
-                switch newValue {
-                case let compound as CompoundPredicate:
-                    primitiveValue = try JSONEncoder.default.encode(compound)
-                case let comparison as ComparisonPredicate:
-                    primitiveValue = try JSONEncoder.default.encode(comparison)
-                default:
-                    let context = EncodingError.Context(codingPath: [], debugDescription: "Unexpected predicate type appeared during encode.")
-                    throw EncodingError.invalidValue(newValue, context)
-                }
-            } catch {
-                os_log("Unable to encode predicate for storage in core data: %@", log: .persistence, type: .error, String(describing: error))
-                return
-            }
-            
-            setPrimitiveValue(primitiveValue, forKey: key)
+    public private(set) var eventAttributeFilterPredicate: Predicate? {
+        get {
+            return getPredicateForPrimitiveField(forKey: eventAttributeFilterPredicateFieldName)
+        }
+        set {
+            setPrimitiveValue(newValue, forKey: eventAttributeFilterPredicateFieldName)
         }
     }
     
+    private func getPredicateForPrimitiveField(forKey key: String) -> Predicate? {
+        self.willAccessValue(forKey: key)
+        defer { self.didAccessValue(forKey: key) }
+        guard let primitiveValue = primitiveValue(forKey: key) as? Data else {
+            return nil
+        }
+        
+        guard let predicateType = try? JSONDecoder.default.decode(PredicateType.self, from: primitiveValue) else {
+            os_log("Unable to determine type of predicate stored in Core Data.", log: .persistence, type: .error)
+            return nil
+        }
+        
+        do {
+            switch predicateType {
+            case .comparisonPredicate:
+                return try JSONDecoder.default.decode(ComparisonPredicate.self, from: primitiveValue)
+            case .compoundPredicate:
+                return try JSONDecoder.default.decode(CompoundPredicate.self, from: primitiveValue)
+            }
+        } catch {
+            os_log("Unable to decode predicate stored in core data: %s", log: .persistence, type: .error, String(describing: error))
+            return nil
+        }
+    }
+    
+    private func setPredicateForPrimitiveField(newValue: Predicate?, forKey key: String) {
+        let key = eventAttributeFilterPredicateFieldName
+        willChangeValue(forKey: key)
+        defer { didChangeValue(forKey: key) }
+        let primitiveValue: Data
+        
+        guard let newValue = newValue else {
+            setPrimitiveValue(nil, forKey: key)
+            return
+        }
+        
+        do {
+            switch newValue {
+            case let compound as CompoundPredicate:
+                primitiveValue = try JSONEncoder.default.encode(compound)
+            case let comparison as ComparisonPredicate:
+                primitiveValue = try JSONEncoder.default.encode(comparison)
+            default:
+                let context = EncodingError.Context(codingPath: [], debugDescription: "Unexpected predicate type appeared during encode.")
+                throw EncodingError.invalidValue(newValue, context)
+            }
+        } catch {
+            os_log("Unable to encode predicate for storage in core data: %@", log: .persistence, type: .error, String(describing: error))
+            return
+        }
+        
+        setPrimitiveValue(primitiveValue, forKey: key)
+    }
+    
+    private func getOptionalBooleanForPrimitiveField(forKey key: String) -> Bool? {
+        let key = timeOfDayFilterStartTimeFieldName
+        self.willAccessValue(forKey: key)
+        defer { self.didAccessValue(forKey: key) }
+        guard let primitiveValue = primitiveValue(forKey: key) as? NSNumber else {
+            return nil
+        }
+        return primitiveValue != 0
+    }
+    
+    private func setOptionalBooleanForPrimitiveField(newValue: Bool?, forKey key: String) {
+        let key = timeOfDayFilterStartTimeFieldName
+        willChangeValue(forKey: key)
+        defer { didChangeValue(forKey: key) }
+        
+        guard let newValue = newValue else {
+            setPrimitiveValue(nil, forKey: key)
+            return
+        }
+        setPrimitiveValue(NSNumber(booleanLiteral: newValue), forKey: key)
+    }
+    
+    
+    // TODO: screw it, turn these into an "Attributes" enum of some sort, use automatic string value to produce key strings.
     private let eventAttributeFilterPredicateFieldName = "eventAttributeFilterPredicate"
+    private let triggerSegmentPredicateFieldName = "triggerSegmentPredicate"
+    private let timeOfDayFilterStartTimeFieldName = "timeOfDayFilterStartTime"
+    private let dayOfWeekFilterMondayFieldName = "dayOfWeekFilterMonday"
 }

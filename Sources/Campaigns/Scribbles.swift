5f089e3ce267928792f300ec0756b96134c18890
Andrew Clunis <andrew@orospakr.ca>
2019-02-12 20:19:22 -0500

    progress on predicate mapping.

--- Sources/Campaigns/Scribbles.swift
+++ Sources/Campaigns/Scribbles.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import os
 
 class CampaignUpdateObserver {
     
@@ -40,7 +41,7 @@ extension AutomatedCampaign: Segmentable {
 
 extension ScheduledCampaign: Segmentable {
     var segment: SegmentModel {
-        fatalError("stand-in")
+        fatalError("Coming later!")
     }
 }
 
@@ -51,22 +52,106 @@ extension Array where Element == Segmentable {
     }
 }
 
-struct CampaignNotificationPipeline {
-    // whenever sync completes:
-    let eventObserver: NSObjectProtocol?
-    
-    init(
-        eventPipeline: EventPipeline
-    ) {
-        self.eventObserver = eventPipeline.observers.add { event in
-            // STEP 1. Query for applicable scheduled campaigns and applicable automated campaigns.
-            
-            
-            
-            // STEP 2.
+extension Predicate {
+    func nsPredicate(forDevice deviceSnapshot: DeviceSnapshot) -> NSPredicate {
+        let nsPredicate: NSPredicate
+
+        switch self {
+        case let comparisonPredicate as ComparisonPredicate:
+            nsPredicate = comparisonPredicate.nsPredicate(forDevice: deviceSnapshot)
+        case let compoundPredicate as CompoundPredicate:
+            nsPredicate = compoundPredicate.nsPredicate(forDevice: deviceSnapshot)
+        default:
+            nsPredicate = NSPredicate()
         }
 
+        return nsPredicate
     }
-    
-    private func 
+}
+
+extension ComparisonPredicate {
+    func nsPredicate() -> NSPredicate {
+        // Left-hand-side value will be the item being tested (as given by keypath)
+        // Right-hand-side value is the constant value given
+        
+        let rightExpression = NSExpression(forKeyPath: self.keyPath)
+        
+        // can't use one big expression here because the Swift compiler lags out.
+        let value1: Any? = self.booleanValue ?? self.booleanValues
+        let value2: Any? = self.dateTimeValue ?? self.dateTimeValues
+        let value3: Any? = self.numberValue ?? self.numberValues
+        let value4: Any? = self.stringValue ?? self.stringValues
+        let value: Any? = value1 ?? value2 ?? value3 ?? value4
+        
+        if value == nil {
+            // TODO: this means the Predicate given from GraphQL was invalid?  Handling this case by matching nothing.
+            os_log("feck", log: .campaigns)
+            return NSPredicate(value: false)
+        }
+        
+        let leftExpression = NSExpression(
+            forConstantValue: value
+        )
+        
+        return NSComparisonPredicate(
+            leftExpression: leftExpression,
+            rightExpression: rightExpression,
+            modifier: modifier.nsModifier,
+            type: `operator`.nsOperator,
+            options: NSComparisonPredicate.Options(rawValue: 0)
+        )
+    }
+}
+
+extension CompoundPredicate {
+    func nsPredicate(forDevice deviceSnapshot: DeviceSnapshot) -> NSPredicate {
+        // ANDREW START HERE
+    }
+}
+
+extension ComparisonPredicateOperator {
+    var nsOperator: NSComparisonPredicate.Operator {
+        switch self {
+        case .lessThan:
+            return .lessThan
+        case .lessThanOrEqualTo:
+            return .lessThanOrEqualTo
+        case .greaterThan:
+            return .greaterThan
+        case .greaterThanOrEqualTo:
+            return .greaterThanOrEqualTo
+        case .equalTo:
+            return .equalTo
+        case .notEqualTo:
+            return .notEqualTo
+        case .like:
+            return .like
+        case .beginsWith:
+            return .beginsWith
+        case .endsWith:
+            return .endsWith
+        case .in:
+            return .in
+        case .contains:
+            return .contains
+        case .between:
+            return .between
+        case .geoWithin:
+            // TODO: uh this has to be handled separately.  An entire compound predicate needs to be constructed with multiple expressions.
+            fatalError("geoWithin operator not yet implemented")
+        }
+    }
+}
+
+extension ComparisonPredicateModifier {
+    var nsModifier: NSComparisonPredicate.Modifier {
+        switch self {
+        case .all:
+            return .all
+        case .any:
+            return .any
+        case .direct:
+            return .direct
+        }
+    }
 }
---
+++ Sources/Data/Model/Predicate.swift
@@ -0,0 +1,116 @@
+//
+//  Predicate.swift
+//  RoverData
+//
+//  Created by Andrew Clunis on 2019-02-12.
+//  Copyright © 2019 Rover Labs Inc. All rights reserved.
+//
+
+import Foundation
+
+public protocol Predicate {
+}
+
+public enum PredicateType: Decodable {
+    case comparisonPredicate
+    case compoundPredicate
+    
+    enum CodingKeys: String, CodingKey {
+        case typeName = "__typename"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let typeName = try container.decode(String.self, forKey: .typeName)
+        switch typeName {
+        case "ComparisonPredicate":
+            self = .comparisonPredicate
+        case "CompoundPredicate":
+            self = .compoundPredicate
+        default:
+            throw DecodingError.dataCorruptedError(forKey: CodingKeys.typeName, in: container, debugDescription: "Expected either ComparisonPredicate or CompoundPredicate – found \(typeName)")
+        }
+    }
+}
+
+public enum ComparisonPredicateModifier: String, Decodable {
+    case direct = "DIRECT"
+    case any = "ANY"
+    case all = "ALL"
+}
+
+public enum ComparisonPredicateOperator: String, Decodable {
+    case lessThan = "LESS_THAN"
+    case lessThanOrEqualTo = "LESS_THAN_OR_EQUAL_TO"
+    case greaterThan = "GREATER_THAN"
+    case greaterThanOrEqualTo = "GREATER_THAN_OR_EQUAL_TO"
+    case equalTo = "EQUAL_TO"
+    case notEqualTo = "NOT_EQUAL_TO"
+    case like = "LIKE"
+    case beginsWith = "BEGINS_WITH"
+    case endsWith = "ENDS_WITH"
+    case `in` = "IN"
+    case contains = "CONTAINS"
+    case between = "BETWEEN"
+    case geoWithin = "GEO_WITHIN"
+}
+
+public enum CompoundPredicateLogicalType: String, Decodable {
+    case or = "OR"
+    case and = "AND"
+    case not = "NOT"
+}
+
+public struct ComparisonPredicate: Predicate, Decodable {
+    public let keyPath: String
+    public let modifier: ComparisonPredicateModifier
+    public let `operator`: ComparisonPredicateOperator
+    public let numberValue: Double? = nil
+    public let numberValues: [Double]? = nil
+    public let stringValue: String? = nil
+    public let stringValues: [String]? = nil
+    public let booleanValue: Bool? = nil
+    public let booleanValues: [Bool]? = nil
+    public let dateTimeValue: Date? = nil
+    public let dateTimeValues: [Date]? = nil
+    
+    public init(
+        keyPath: String,
+        modifier: ComparisonPredicateModifier,
+        `operator`: ComparisonPredicateOperator
+    ) {
+        self.keyPath = keyPath
+        self.modifier = modifier
+        self.`operator` = `operator`
+    }
+}
+
+public struct CompoundPredicate: Predicate, Decodable {
+    public let booleanOperator: CompoundPredicateLogicalType
+    
+    public let predicates: [Predicate]
+    
+    public enum CodingKeys: String, CodingKey {
+        case booleanOperator
+        case predicates
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.booleanOperator = try container.decode(CompoundPredicateLogicalType.self, forKey: .booleanOperator)
+        
+        var predicatesContainer = try container.nestedUnkeyedContainer(forKey: .predicates)
+        var typePeekContainer = predicatesContainer
+        var predicates = [Predicate]()
+        while !predicatesContainer.isAtEnd {
+            let predicateType = try typePeekContainer.decode(PredicateType.self)
+            switch predicateType {
+            case .comparisonPredicate:
+                predicates.append(try predicatesContainer.decode(ComparisonPredicate.self))
+            case .compoundPredicate:
+                predicates.append(try predicatesContainer.decode(CompoundPredicate.self))
+            }
+        }
+        self.predicates = predicates
+    }
+}

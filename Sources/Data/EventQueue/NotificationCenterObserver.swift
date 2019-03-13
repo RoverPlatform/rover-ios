7bd4e0c6578848c32a926adc81f13861c3726d87
Andrew Clunis <andrew@orospakr.ca>
2019-03-13 08:27:12 -0400

    pencil in observer

---
+++ Sources/Data/EventQueue/NotificationCenterObserver.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationCenterObserver.swift
+//  RoverCampaignsData
+//
+//  Created by Andrew Clunis on 2019-03-12.
+//  Copyright © 2019 Rover Labs Inc. All rights reserved.
+//
+
+import Foundation
+
+class NotificationCenterObserver {
+    
+}

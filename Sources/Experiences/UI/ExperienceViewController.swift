8c71a67b005fb21a52a94c05baf29546882ec9e5
Andrew Clunis <andrew@orospakr.ca>
2019-03-14 20:56:46 -0400

    Rename ExperienceContainer to ExperienceViewController, to promote it as entry point exposed to developers.

--- Sources/Experiences/UI/ExperienceViewController.swift
+++ Sources/Experiences/UI/ExperienceViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-open class ExperienceContainer: UIViewController {
+open class ExperienceViewController: UIViewController {
     public let identifier: ExperienceIdentifier
     public let store: ExperienceStore
     

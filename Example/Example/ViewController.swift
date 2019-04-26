419366f07ef0481977ca611abc05c3c665a390e7
Andrew Clunis <andrew@rover.io>
2019-04-26 15:13:30 -0400

    Generate example app

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Andrew Clunis on 2019-04-26.
+//  Copyright © 2019 Rover Labs Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
